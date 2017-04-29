5f59075b6a6e200713dde574815bd5c272ebb5fb
obscuri <szymonkopryk@gmail.com>
2017-04-29 18:45:36 +0200

    1

---
+++ Figury/pion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja1.figury
+{
+    class Pion : figura
+    {
+        public pion(Pozycja pozycja, string kolor) : base (pozycja,kolor)
+        {
+
+        }
+        public virtual bool CzyMoznaPrzesunac(Pozycja nowaPozycja)
+        {
+            return CzyPionowo(nowaPozycja) || CzyPoziomo(nowaPozycja);
+        }
+    }
+}
